96c1e3e8c36a0e95715e5bd4df8f970748bb29c6
Jason Morgan <jmorgan@f9vs.com>
2018-06-10 16:46:18 -0600

    Trying an alternative input syntax.

--- ci/deploy-opsman/tasks/create-infrastructure/task.sh
+++ ci/deploy-opsman/tasks/create-infrastructure/task.sh
@@ -9,7 +9,7 @@ pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d'
 # ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
 pcf_opsman_image_name=$(echo $pcf_opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
 
-export GOOGLE_CREDENTIALS=${GCP_SERVICE_ACCOUNT_KEY}
+export TF_VAR_service_account_key=${GCP_SERVICE_ACCOUNT_KEY}
 export GOOGLE_PROJECT=${GCP_PROJECT_ID}
 export GOOGLE_REGION=${GCP_REGION}
 
@@ -21,7 +21,6 @@ terraform plan \
   -var "env_prefix=${GCP_RESOURCE_PREFIX}" \
   -var "pcf_opsman_image_name=${pcf_opsman_image_name}" \
   -var "project=${GCP_PROJECT_ID}" \
-  -var "service_account_key=${GCP_SERVICE_ACCOUNT_KEY}" \
   -var "nat_machine_type=n1-standard-4" \
   -var "opsman_machine_type=n1-standard-2" \
   -var "domain=${GCP_DOMAIN}" \
@@ -29,6 +28,8 @@ terraform plan \
   -out terraform.tfplan \
   -state terraform-state/terraform.tfstate \
   pcf-pipelines/install-pcf/gcp/terraform
+
+# -var "service_account_key=${GCP_SERVICE_ACCOUNT_KEY}" \
 
 terraform apply \
   -state-out $root/create-infrastructure-output/terraform.tfstate \
